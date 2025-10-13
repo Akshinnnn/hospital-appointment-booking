d65c973c7adf54872b62b7bef3c4c07deb1feedc
Akshinnnn <akshin.mashadiyev.02@gmail.com>
2025-10-13 15:34:51 +0300

    fixed cors problem with port

--- backend/ApiGateway/Program.cs
+++ backend/ApiGateway/Program.cs
@@ -6,7 +6,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173") // frontend URL
+        policy.WithOrigins("http://localhost:3000") // frontend URL
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
